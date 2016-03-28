3fc5bcd063d0f3863c87d38613b4f5f38258006a
prestonw <wpreston@longviewinc.com>
2016-03-27 21:13:49 -0400

    Playlist scrolls to playing song item so its always in view. User can click on artist avatar or progress bar to toggle play state of song item in playlist.

--- www/app/directives/song.js
+++ www/app/directives/song.js
@@ -11,189 +11,199 @@ angular.module('audiodio.directives.song', ['angular-inview'])
     templateUrl: 'app/directives/song.html'
   };
 }])
-.controller('songCtrl', ['links', '$scope', '$stateParams', 'xipath', '$state', 'session', '$timeout', 'metrics', 'user', '$rootScope', 'artistCovers',
-  function (links, $scope , $stateParams, xipath, $state, session, $timeout, metrics, user, $rootScope, artistCovers) {
-    //model get/set
-    var vm = this;
-    vm.showSong = showSong;
-    vm.removeSong = removeSong;
-    //audio tag controls
-    vm.getCurrentTime = getCurrentTime;
-    vm.currentTime = {};
-    vm.currentTime.val = 0;
-    vm.duration = 0;
-    vm.renderThread = setInterval(function () {
-      var $audio = document.getElementById(xipath.getContext());
-      if ($audio) {
-        $timeout(function () {
-          vm.currentTime.val  = $audio.currentTime;
-          vm.duration = $audio.duration;
-          $scope.$apply();
-        });
-      }
-    }, 2 * 1000);
-    vm.isSongLoaded  = false;
-    vm.isSongPlaying = isSongPlaying;
-    vm.load = load;
-    vm.play = play;
-    vm.pause = pause;
-    vm.togglePlayingState = togglePlayingState;
-    vm.next = next;
-    vm.isSongInContext = isSongInContext;
-    vm.volume = 50.0;
+.controller('songCtrl', SongCtrl)
+.filter('encodeReservedCharacters', function () {
+  return function (uri) {
+    return encodeURIComponent(uri);
+  };
+})
+.filter('artistNameFilter', function () {
+  return function (model) {
+    return model.artist.substring(3, model.artist.length);
+  };
+})
+.filter('albumNameFilter', function () {
+  return function (model) {
+    return model.album.substring(3, model.album.length);
+  };
+});
 
-    vm.artist = {
-      xipath: '',
-      image: "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-    };
-    vm.song = {
-      "xipath": "",
-      "bitrate":"",
-      "duration":0,
-      "name":"",
-      "album":"",
-      "artist":"",
-      "genre":"",
-      "year":"",
-      "uri":""
-    };
-    vm.song = angular.extend(vm.song, $scope.model); //passed into directive
-
-    showMetaData(); //get cached data or fetch GET data
-
-    function showMetaData() {
-
-        //load artist meta data
-        links.formUrl('artistMetaData').then(function (url) {
-          artistCovers.fetchArtistMetaData(url, vm.song.xipath.substring(0,6)).then(function (artist) {
-            vm.artist = artist;
-          });
-        });
-
-        //Load song into context
-        if (!vm.song.uri ||vm.song.uri.length === 0) {
-          links.formUrl('loadSong').then(function (url) {
-            xipath.fetchSongByXipath(url, vm.song.xipath).then(function (song) {
-              setTimeout(function () {
-                $scope.$apply(function () {
-                  vm.song = song;
-
-                  vm.isSongLoaded = true;
-                });
-              }, 1);
-            });
-          });
-        } else {
-          vm.isSongLoaded = true;
-        }
-    }
-    //playlist methods
-    function showSong (s) {
-      xipath.setContext(s.xipath);
-    }
-
-    function removeSong (s) {
-      session.removeSongByXipath(s.xipath);
-
-      links.formUrl('savePlaylist').then(function (url) {
-        session.resetPlaylistName();
-        session.savePlaylist(url, user.getId()).then(function (success) {
-          //success is boolean
-        });
-      });
-
-      clearInterval(vm.renderThread);
-    }
-
-    function getCurrentTime () {
-      var $audio = document.getElementById(xipath.getContext());
-      if (vm.isSongInContext()) {
-        return $audio.currentTime;
-      } else {
-        return 0;
-      }
-    }
-
-
-    function isSongPlaying () {
-      var $audio = document.getElementById(xipath.getContext());
-      if ($audio) {
-        return !$audio.paused;
-      } else {
-        return false;
-      }
-    }
-
-    function load () {
-      var $audio = document.getElementById(xipath.getContext());
-      if ($audio) {
-        $audio.load();
-        $audio.play();
-        $audio.addEventListener('ended', vm.next);
-      }
-
-      $scope.$emit('song:load');
-
-      links.formUrl('recordPlay').then(function (url) {
-        metrics.recordSongPlayedByXipath(url, vm.song.xipath, user.getId()).then(function (success) {
-          if (success) {
-            console.log('recording song being played'); //TESTING!!!
-          } else {
-            console.log('could not record song being played'); //TESTING!!!
-          }
-        })
-      });
-    }
-    function play () {
-      var $audio = document.getElementById(xipath.getContext());
-      $audio.volume = vm.volume / 100.0;
-      if ($audio && $audio.currentTime === 0) {
-        vm.load();
-      } else if ($audio) {
-        $audio.play();
-      }
-    }
-    function pause () {
-      var $audio = document.getElementById(xipath.getContext());
-      if ($audio) {
-        $audio.pause();
-      }
-    }
-    function togglePlayingState () {
-      if (vm.isSongPlaying()) {
-        vm.pause();
-      } else {
-        vm.play();
-      }
-    }
-    function next () {
-      console.log('attempting to skip to next song'); //TESTING!!!
-      var nxt = session.getNextXipath(xipath.getContext());
-
+SongCtrl.$inject = ['links', '$scope', 'xipath', 'session', '$timeout', 'metrics', 'user', 'artistCovers', '$ionicScrollDelegate'];
+function SongCtrl (links, $scope , xipath, session, $timeout, metrics, user, artistCovers, $ionicScrollDelegate) {
+  //model get/set
+  var vm = this;
+  vm.showSong = showSong;
+  vm.removeSong = removeSong;
+  //audio tag controls
+  vm.getCurrentTime = getCurrentTime;
+  vm.currentTime = {};
+  vm.currentTime.val = 0;
+  vm.duration = 0;
+  vm.renderThread = setInterval(function () {
+    var $audio = document.getElementById(xipath.getContext());
+    if ($audio) {
       $timeout(function () {
-        xipath.setContext(nxt);
+        vm.currentTime.val  = $audio.currentTime;
+        vm.duration = $audio.duration;
         $scope.$apply();
-        vm.load();
       });
     }
+  }, 2 * 1000);
+  vm.isSongLoaded  = false;
+  vm.isSongPlaying = isSongPlaying;
+  vm.load = load;
+  vm.play = play;
+  vm.pause = pause;
+  vm.togglePlayingState = togglePlayingState;
+  vm.next = next;
+  vm.isSongInContext = isSongInContext;
+  vm.volume = 50.0;
 
-    function isSongInContext (xi) {
-      return xipath.getContext() === xi;
+  vm.artist = {
+    xipath: '',
+    image: "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+  };
+  vm.song = {
+    "xipath": "",
+    "bitrate":"",
+    "duration":0,
+    "name":"",
+    "album":"",
+    "artist":"",
+    "genre":"",
+    "year":"",
+    "uri":""
+  };
+  vm.song = angular.extend(vm.song, $scope.model); //passed into directive
+
+  showMetaData(); //get cached data or fetch GET data
+
+  function showMetaData() {
+
+      //load artist meta data
+      links.formUrl('artistMetaData').then(function (url) {
+        artistCovers.fetchArtistMetaData(url, vm.song.xipath.substring(0,6)).then(function (artist) {
+          vm.artist = artist;
+        });
+      });
+
+      //Load song into context
+      if (!vm.song.uri ||vm.song.uri.length === 0) {
+        links.formUrl('loadSong').then(function (url) {
+          xipath.fetchSongByXipath(url, vm.song.xipath).then(function (song) {
+            setTimeout(function () {
+              $scope.$apply(function () {
+                vm.song = song;
+
+                vm.isSongLoaded = true;
+              });
+            }, 1);
+          });
+        });
+      } else {
+        vm.isSongLoaded = true;
+      }
+  }
+  //playlist methods
+  function showSong (s) {
+    xipath.setContext(s.xipath);
+    togglePlayingState();
+
+  }
+
+  function removeSong (s) {
+    session.removeSongByXipath(s.xipath);
+
+    links.formUrl('savePlaylist').then(function (url) {
+      session.resetPlaylistName();
+      session.savePlaylist(url, user.getId()).then(function (success) {
+        //success is boolean
+      });
+    });
+
+    clearInterval(vm.renderThread);
+  }
+
+  function getCurrentTime () {
+    var $audio = document.getElementById(xipath.getContext());
+    if (vm.isSongInContext()) {
+      return $audio.currentTime;
+    } else {
+      return 0;
+    }
+  }
+
+
+  function isSongPlaying () {
+    var $audio = document.getElementById(xipath.getContext());
+    if ($audio) {
+      return !$audio.paused;
+    } else {
+      return false;
+    }
+  }
+
+  function load () {
+    var $audio = document.getElementById(xipath.getContext());
+    if ($audio) {
+      //.$getByHandle(xipath.getContext()).scrollTop();
+      $timeout(function () {
+        var $scrollToMe = document.getElementById('scroll#' + xipath.getContext());
+        $ionicScrollDelegate.scrollBy(0, $scrollToMe.offsetTop - $ionicScrollDelegate.getScrollPosition().top, true);
+      }, 1 * 1000);
+      $audio.load();
+      $audio.play();
+      $audio.addEventListener('ended', vm.next);
     }
 
-  }])
-  .filter('encodeReservedCharacters', function () {
-    return function (uri) {
-      return encodeURIComponent(uri);
-    };
-  })
-  .filter('artistNameFilter', function () {
-    return function (model) {
-      return model.artist.substring(3, model.artist.length);
-    };
-  })
-  .filter('albumNameFilter', function () {
-    return function (model) {
-      return model.album.substring(3, model.album.length);
-    };
-  });
+    $scope.$emit('song:load');
+
+    links.formUrl('recordPlay').then(function (url) {
+      metrics.recordSongPlayedByXipath(url, vm.song.xipath, user.getId()).then(function (success) {
+        if (success) {
+          console.log('recording song being played'); //TESTING!!!
+        } else {
+          console.log('could not record song being played'); //TESTING!!!
+        }
+      })
+    });
+  }
+  function play () {
+
+    var $audio = document.getElementById(xipath.getContext());
+    $audio.volume = vm.volume / 100.0;
+    if ($audio && $audio.currentTime === 0) {
+      vm.load();
+    } else if ($audio) {
+      $audio.play();
+    }
+  }
+  function pause () {
+    var $audio = document.getElementById(xipath.getContext());
+    if ($audio) {
+      $audio.pause();
+    }
+  }
+  function togglePlayingState () {
+    if (vm.isSongPlaying()) {
+      vm.pause();
+    } else {
+      vm.play();
+    }
+  }
+  function next () {
+    console.log('attempting to skip to next song'); //TESTING!!!
+    var nxt = session.getNextXipath(xipath.getContext());
+
+    $timeout(function () {
+      xipath.setContext(nxt);
+      $scope.$apply();
+      vm.load();
+    });
+  }
+
+  function isSongInContext (xi) {
+    return xipath.getContext() === xi;
+  }
+
+}
